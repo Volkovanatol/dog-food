57b468c4aa6fe21e8a00f408bb35025f6e6e8749
Алёна <s.piter2003@mail.ru>
2023-02-05 20:08:52 +0300

    five

--- src/Api.js
+++ src/Api.js
@@ -30,16 +30,59 @@ class Api {
             }
         })
     }
-
-
-    getProducts(id) {
+    getProduct(id) {
         return fetch(`${this.path}/products/${id}`, {
-             headers: {
-                 "authorization": `Bearer ${this.token}`
-        }
+            headers: {
+                "authorization": `Bearer ${this.token}`
+            }
+        })
+    }
+    addProduct(body) {
+        return fetch(`${this.path}/products`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": `Bearer ${this.token}`
+            },
+            body: JSON.stringify(body)
+        });
+    }
+    delProduct(id) {
+        return fetch(`${this.path}/products/${id}`, {
+            method: "DELETE",
+            headers: {
+                "authorization": `Bearer ${this.token}`
+            }
+        })
+    }
+    setLike(id, isLike) {
+        return fetch(`${this.path}/products/likes/${id}`, {
+            method: isLike ? "DELETE" : "PUT",
+            headers: {
+                "authorization": `Bearer ${this.token}`
+            }
+        })
+    }
+    setReview(productId, body) {
+        return fetch(`${this.path}/products/review/${productId}`, {
+            method: "POST",
+            headers: {
+                "authorization": `Bearer ${this.token}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        })
+    }
+    updUser(body, img = false){
+        return fetch(`${this.path}/v2/${this.group}/users/me${img ? "/avatar" : ""}`, {
+        method: "PATCH",
+        headers: {
+            "authorization": `Bearer ${this.token}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body)
     })
-}
-
+    }
 }
 
 export {Api};
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,11 +1,18 @@
 import React, {useContext} from "react";
 import {Link} from "react-router-dom";
 import Search from "../Search/search";
-import "./header.css";
 import Ctx from "../../Ctx";
+import { PlusCircle, HeartFill, Cart2 } from "react-bootstrap-icons";
+import {Badge} from "react-bootstrap";
+import Basket from "../../pages/Basket";
+
+import "./header.css";
+
+import logo from "../Logo/logo.svg";
+import Favorites from "../../pages/Favorites";
 
 export default () => {
-    const {user, setUser, setModalActive, PATH, favorites} = useContext(Ctx);
+    const {user, setUser, setModalActive, PATH, favorites,  basket} = useContext(Ctx);
     // хук состояния [свойство, функция в качестве аргумента которой передается новое значение нашего свойства] = useState(аргумент - изначальное значение свойства)
     // const [user, setUser] = useState(localStorage.getItem("user8"));
 
@@ -20,16 +27,34 @@ export default () => {
         setUser("");
     }
     return <header>
-        <Link className="logo" to="/">DogFood</Link>
+    <div className="header-top">
+        <Link className="logo" to={PATH}>
+            <img src={logo} />
+        </Link>
         <Search/>
-        {/* <input type="search" placeholder="Поиск..." className="search"/> */}
         <nav className="menu">
-            {/* true && true */}
-            {user && user.name && <Link to="/profile">{user.name}</Link>}
+            {user && <Link to={PATH + "add"}><PlusCircle style={{fontSize: "20px"}}/></Link>}
+            {user && <Link to={PATH + "favorites"} className="badge-link">
+                <HeartFill style={{fontSize: "20px"}}/>
+                <Badge bg="light" text="dark">{favorites.length}</Badge>
+            </Link>}
+            {user && <Link to={PATH + "basket"} className="badge-link">
+                <Cart2 style={{fontSize: "20px"}}/>
+                <Badge bg="light" text="dark">{basket.reduce((acc, el) => acc + el.cnt, 0)}</Badge>
+            </Link>}
+            {user && user.name && <Link to={PATH + "profile"}>{user.name}</Link>}
             {!user && <a href="" onClick={logIn}>Войти</a>}
-            {user && <a href="" onClick={logOut}>Выйти</a>}
+            {/* {user && <a href="" onClick={logOut}>Выйти</a>} */}
         </nav>
-    </header>
+        </div>
+        <div className="header-bottom">
+            <h1>Kакомства для собак</h1>
+            <h3>Всегда свежие лакомства ручной работы с доставкой на дом по России</h3>
+        <Link className="btn btn-secondary btn-lg" to="/catalog">
+            Каталог
+        </Link>
+    </div>
+</header>
 }
 
 // header, footer, main, section, nav, aside, article => div
